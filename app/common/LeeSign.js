bb1fe0867a896c431fde426371e7ee5be8bccad8
springcomefromprogrammingtheworld <466232689@qq.com>
2023-04-08 04:04:18 +0800

    Update LeeSign.js
    修改

--- app/common/LeeSign.js
+++ app/common/LeeSign.js
@@ -19,7 +19,7 @@ exports.verifySign = function(req,res,next,callback){
             form.encoding = 'utf-8';        //设置编辑
             form.uploadDir =dir;     //设置上传目录
             form.keepExtensions = true;     //保留后缀
-            form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
+            form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小.
             form.parse(req, function (err, fields, files) {
                 if (err) {
                     res.locals.error = err;
