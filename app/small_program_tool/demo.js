d7830796d1bf3f660b0c1e91f89ebbb9907b5146
springcomefromprogrammingtheworld <466232689@qq.com>
2023-04-08 04:14:57 +0800

    Update demo.js
    修改

--- app/small_program_tool/demo.js
+++ app/small_program_tool/demo.js
@@ -1,26 +1,9 @@
 var WXBizDataCrypt = require('./WXBizDataCrypt')
 
-var appId = 'wx4f4bc4dec97d474b'
-var sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
-var encryptedData = 
-	'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM'+
-	'QmRzooG2xrDcvSnxIMXFufNstNGTyaGS'+
-	'9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+'+
-	'3hVbJSRgv+4lGOETKUQz6OYStslQ142d'+
-	'NCuabNPGBzlooOmB231qMM85d2/fV6Ch'+
-	'evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6'+
-	'/1Xx1COxFvrc2d7UL/lmHInNlxuacJXw'+
-	'u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn'+
-	'/Hz7saL8xz+W//FRAUid1OksQaQx4CMs'+
-	'8LOddcQhULW4ucetDf96JcR3g0gfRK4P'+
-	'C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB'+
-	'6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns'+
-	'/8wR2SiRS7MNACwTyrGvt9ts8p12PKFd'+
-	'lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV'+
-	'oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG'+
-	'20f0a04COwfneQAGGwd5oa+T8yO5hzuy'+
-	'Db/XcxxmK01EpqOyuxINew=='
-var iv = 'r7BXXKkLb8qrSNn05n0qiA=='
+var appId = '';
+var sessionKey = '';
+var encryptedData = '';
+var iv = '';
 
 var pc = new WXBizDataCrypt(appId, sessionKey)
 
@@ -40,6 +23,6 @@ console.log('解密后 data: ', data)
 //   "unionId": "ocMvos6NjeKLIBqg5Mr9QjxrP1FA",
 //   "watermark": {
 //     "timestamp": 1477314187,
-//     "appid": "wx4f4bc4dec97d474b"
+//     "appid": ""
 //   }
 // }
